9ddd89eed32df9886e29e8a58296f3ef0da8a6ff
adevgriff <adevgriff@gmail.com>
2024-03-28 22:35:56 -0400

    Added JMP and JG tests

--- Test/test_code/instructions/JG/JG.asm
+++ Test/test_code/instructions/JG/JG.asm
@@ -3,18 +3,18 @@
 
 !test_one
     cmp rA, 0
+    JG !fail
     JMP !success_one
-    set rB, 0
 
 !test_two
     cmp rA, -1
-    JMP !success_two
-    set rB, 0
+    JG !success_two
+    JMP !fail
 
 !test_three
     cmp rA, 1
+    JG !fail
     JMP !success_three
-    set rB, 0
 
 !success_one
     JMP !test_two
